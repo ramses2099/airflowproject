52ba156b49c022eb8edea65b1e805038db86076a
ramses2099 <ramses2099@gmail.com>
2024-06-27 14:47:31 -0400

    This is a commit changes repo airflowproject date 2024-06-27T14:47:31

---
+++ dags/dag_with_postgress_operator.py
@@ -0,0 +1,39 @@
+from datetime import datetime, timedelta
+
+from airflow.decorators import dag, task
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+
+
+default_args ={
+    'owner':'ramses2099',
+    'retries':5,
+    'retry_delay':timedelta(minutes=5)
+}
+
+@dag(default_args = default_args,
+    dag_id='our_dag_with_postgree_operator',
+    description = 'Our first dag using postgress operator',
+    start_date=datetime(2024, 6, 26, 2),
+    schedule_interval = '@daily',
+)
+def import_data_dbticketsystem_etl():
+    
+    task1 = PostgresOperator(
+            task_id="insert_data_employees_table",
+            postgres_conn_id="postress_connection",
+            sql="""
+            insert into api_departament (name, created, user_id) values ('Business Development', '2023-11-21', 1);
+            insert into api_departament (name, created, user_id) values ('Product Management', '2023-08-31', 1);
+            insert into api_departament (name, created, user_id) values ('Accounting', '2023-07-17', 1);
+            insert into api_departament (name, created, user_id) values ('Human Resources', '2024-05-29', 1);
+            insert into api_departament (name, created, user_id) values ('Accounting', '2023-11-17', 1);
+            insert into api_departament (name, created, user_id) values ('Research and Development', '2023-12-03', 1);
+            insert into api_departament (name, created, user_id) values ('Services', '2023-08-02', 1);
+            insert into api_departament (name, created, user_id) values ('Business Development', '2024-01-03', 1);
+            insert into api_departament (name, created, user_id) values ('Research and Development', '2024-05-06', 1);
+            insert into api_departament (name, created, user_id) values ('Training', '2023-07-27', 1);
+            """,
+        )
+    task1()
+    
+import_data_dbticketsystem_etl()
