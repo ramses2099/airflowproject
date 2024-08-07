3b3a0ca1276afb0ddf429ad1def1c48cb645224b
ramses2099 <ramses2099@gmail.com>
2024-08-07 14:24:31 -0400

    new dag

--- dags/dag_test.py
+++ dags/dag_test.py
@@ -14,26 +14,36 @@ default_args ={
     dag_id='our_dag_with_postgree_operator',
     description = 'Our first dag using postgress operator',
     start_date=datetime(2024, 8, 7),
-    schedule_interval = '@hourly',
+    schedule_interval = '@once',
+    catchup = False,
 )
 def import_data_dbticketsystem_etl():
-
     task1 = PostgresOperator(
-            task_id="insert_data_departament_table",
+            task_id="create_pet_table",
             postgres_conn_id="postress_connection",
             sql="""
-            insert into api_departament (name, created, user_id) values ('Business Development', '2023-11-21', 1);
-            insert into api_departament (name, created, user_id) values ('Product Management', '2023-08-31', 1);
-            insert into api_departament (name, created, user_id) values ('Accounting', '2023-07-17', 1);
-            insert into api_departament (name, created, user_id) values ('Human Resources', '2024-05-29', 1);
-            insert into api_departament (name, created, user_id) values ('Accounting', '2023-11-17', 1);
-            insert into api_departament (name, created, user_id) values ('Research and Development', '2023-12-03', 1);
-            insert into api_departament (name, created, user_id) values ('Services', '2023-08-02', 1);
-            insert into api_departament (name, created, user_id) values ('Business Development', '2024-01-03', 1);
-            insert into api_departament (name, created, user_id) values ('Research and Development', '2024-05-06', 1);
-            insert into api_departament (name, created, user_id) values ('Training', '2023-07-27', 1);
+                CREATE TABLE IF NOT EXISTS pet (
+                pet_id SERIAL PRIMARY KEY,
+                name VARCHAR NOT NULL,
+                pet_type VARCHAR NOT NULL,
+                birth_date DATE NOT NULL,
+                OWNER VARCHAR NOT NULL);            
             """,
         )
-    [task1]
+    task2 = PostgresOperator(
+            task_id="create_pet_table",
+            postgres_conn_id="postress_connection",
+            sql="""
+                INSERT INTO pet (name, pet_type, birth_date, OWNER)
+                VALUES ( 'Max', 'Dog', '2018-07-05', 'Jane');
+                INSERT INTO pet (name, pet_type, birth_date, OWNER)
+                VALUES ( 'Susie', 'Cat', '2019-05-01', 'Phil');
+                INSERT INTO pet (name, pet_type, birth_date, OWNER)
+                VALUES ( 'Lester', 'Hamster', '2020-06-23', 'Lily');
+                INSERT INTO pet (name, pet_type, birth_date, OWNER)
+                VALUES ( 'Quincy', 'Parrot', '2013-08-11', 'Anne');           
+            """,
+        )
+    [task1,task2]
     
 import_data_dbticketsystem_etl()
